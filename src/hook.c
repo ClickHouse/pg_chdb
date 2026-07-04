a5e7e044cccb0a55275cc166fac504ebe6344d0e
David E. Wheeler <david.wheeler@clickhouse.com>
2026-07-04 11:31:27 -0400

    Send context strings to worker via shared memory
    One cannot send pointers via `bgw_extra`, and even if one could, it's
    not large enough for all of the parameters to be passed.
    
    Instead, pass only the database and role OIDs and the scheme via
    `bgw_extra` and allocate a shared memory segment of the appropriate size
    to hold all the string values and send them via the segment. This is a
    bit fussy but it works well.
    
    Also borrow from the Postgres parallel worker code and set up a memory
    context in the worker "just for cleanliness".

--- src/hook.c
+++ src/hook.c
@@ -2,12 +2,17 @@
 #include "postgres.h"
 
 #include "bgw/worker.h"
+#include "catalog/namespace.h"
 #include "miscadmin.h"
 #include "nodes/pg_list.h"
 #include "postmaster/bgworker.h"
+#include "storage/dsm.h"
+#include "storage/shm_toc.h"
 #include "tcop/cmdtag.h"
 #include "tcop/utility.h"
 #include "utils/builtins.h"
+#include "utils/lsyscache.h"
+#include "utils/rel.h"
 
 /* previous hook */
 static ProcessUtility_hook_type PrevProcessUtility = NULL;
@@ -68,6 +73,15 @@ scheme_for(const char* str) {
 static void
 contextualize_options(chdbCopyContext* ctx, List* options) {
     ListCell* lc;
+    ctx->access_key        = "";
+    ctx->access_secret     = "";
+    ctx->session_token     = "";
+    ctx->format            = "";
+    ctx->structure         = "";
+    ctx->compression       = "";
+    ctx->headers           = "";
+    ctx->extra_credentials = "";
+
     foreach (lc, options) {
         DefElem* elem = (DefElem*)lfirst(lc);
         char* pname   = elem->defname;
@@ -121,14 +135,19 @@ chDBProcessUtilityHook(
         scheme scheme  = scheme_for(copy->filename);
         if (scheme != no_scheme) {
             /* We own this copy. Fire up a worker to execute it. */
+            char* schema = copy->relation->schemaname
+                               ? copy->relation->schemaname
+                               : get_namespace_name(get_rel_namespace(
+                                     RangeVarGetRelid(copy->relation, NoLock, true)
+                                 ));
             chdbCopyContext ctx = {
                 .scheme  = scheme,
-                .dboid   = MyDatabaseId,
-                .roleoid = GetAuthenticatedUserId(),
+                .db_id   = MyDatabaseId,
+                .role_id = GetAuthenticatedUserId(),
                 // .session_user_id = GetSessionUserId(),
                 // .outer_user_id = GetCurrentRoleId(),
                 .table  = copy->relation->relname,
-                .schema = copy->relation->schemaname,
+                .schema = schema,
                 .url    = copy->filename,
             };
             contextualize_options(&ctx, copy->options);
@@ -165,33 +184,116 @@ chDBProcessUtilityHook(
  */
 void
 LaunchWorker(chdbCopyContext* ctx) {
-    BackgroundWorker worker;
-    BackgroundWorkerHandle* handle;
-    BgwHandleStatus status;
-    pid_t pid;
+    /* Size the shared memory for the chdbCopyContext. */
+    shm_toc_estimator estimator;
 
+    shm_toc_initialize_estimator(&estimator);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->schema) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->table) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->url) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->access_key) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->access_secret) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->session_token) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->format) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->structure) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->compression) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->headers) + 1);
+    shm_toc_estimate_chunk(&estimator, strlen(ctx->extra_credentials) + 1);
+    shm_toc_estimate_keys(&estimator, 11);
+
+    /* Create the shared memory segment. */
+    Size seg_size    = shm_toc_estimate(&estimator);
+    dsm_segment* seg = dsm_create(seg_size, 0);
+
+    /*
+     * Copy the context strings into the shared memory segment.
+     */
+    shm_toc* toc = shm_toc_create(CHDB_SHM_MAGIC, dsm_segment_address(seg), seg_size);
+
+    char* string_shm = shm_toc_allocate(toc, strlen(ctx->schema) + 1);
+    strcpy(string_shm, ctx->schema);
+    shm_toc_insert(toc, CHDB_KEY_SCHEMA, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->table) + 1);
+    strcpy(string_shm, ctx->table);
+    shm_toc_insert(toc, CHDB_KEY_TABLE, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->url) + 1);
+    strcpy(string_shm, ctx->url);
+    shm_toc_insert(toc, CHDB_KEY_URL, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->access_key) + 1);
+    strcpy(string_shm, ctx->access_key);
+    shm_toc_insert(toc, CHDB_KEY_ACCESS_KEY, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->access_secret) + 1);
+    strcpy(string_shm, ctx->access_secret);
+    shm_toc_insert(toc, CHDB_KEY_ACCESS_SECRET, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->session_token) + 1);
+    strcpy(string_shm, ctx->session_token);
+    shm_toc_insert(toc, CHDB_KEY_SESSION_TOKEN, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->format) + 1);
+    strcpy(string_shm, ctx->format);
+    shm_toc_insert(toc, CHDB_KEY_FORMAT, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->structure) + 1);
+    strcpy(string_shm, ctx->structure);
+    shm_toc_insert(toc, CHDB_KEY_STRUCTURE, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->compression) + 1);
+    strcpy(string_shm, ctx->compression);
+    shm_toc_insert(toc, CHDB_KEY_COMPRESSION, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->headers) + 1);
+    strcpy(string_shm, ctx->headers);
+    shm_toc_insert(toc, CHDB_KEY_HEADERS, string_shm);
+
+    string_shm = shm_toc_allocate(toc, strlen(ctx->extra_credentials) + 1);
+    strcpy(string_shm, ctx->extra_credentials);
+    shm_toc_insert(toc, CHDB_KEY_EXTRA_CREDENTIALS, string_shm);
+
+    /* Create the worker. */
+    BackgroundWorker worker;
     memset(&worker, 0, sizeof(worker));
     worker.bgw_flags = BGWORKER_SHMEM_ACCESS | BGWORKER_BACKEND_DATABASE_CONNECTION;
-
     worker.bgw_start_time   = BgWorkerStart_RecoveryFinished;
     worker.bgw_restart_time = BGW_NEVER_RESTART;
     sprintf(worker.bgw_library_name, "chdb_bgw");
     sprintf(worker.bgw_function_name, "chdb_bgw_main");
-    snprintf(
-        worker.bgw_name, BGW_MAXLEN, "chdb_bgw %s.%s worker", ctx->schema, ctx->table
-    );
+    snprintf(worker.bgw_name, BGW_MAXLEN, "chdb %s.%s worker", ctx->schema, ctx->table);
     snprintf(worker.bgw_type, BGW_MAXLEN, "chdb_bgw dynamic");
-    /* set bgw_notify_pid so that we can use WaitForBackgroundWorkerStartup */
+    worker.bgw_main_arg   = UInt32GetDatum(dsm_segment_handle(seg));
     worker.bgw_notify_pid = MyProcPid;
-    memcpy(worker.bgw_extra, ctx, sizeof(chdbCopyContext));
 
+    /* Send the fixed size values via bgw_extra. */
+    char* p = worker.bgw_extra;
+    memcpy(p, &ctx->db_id, sizeof(Oid));
+    p += sizeof(Oid);
+    memcpy(p, &ctx->role_id, sizeof(Oid));
+    p += sizeof(Oid);
+    memcpy(p, &ctx->scheme, sizeof(scheme));
+
+    /* Register the worker. */
+    BackgroundWorkerHandle* handle;
     if (!RegisterDynamicBackgroundWorker(&worker, &handle)) {
-        return;
+        dsm_detach(seg);
+        /* Wording copied from Postgres source, will be localized. */
+        ereport(
+            ERROR,
+            errcode(ERRCODE_CONFIGURATION_LIMIT_EXCEEDED),
+            errmsg("out of background worker slots"),
+            errhint("You might need to increase max_worker_processes.")
+        );
     }
 
-    status = WaitForBackgroundWorkerStartup(handle, &pid);
+    /* Wait for the background worker to start */
+    pid_t pid;
+    BgwHandleStatus status = WaitForBackgroundWorkerStartup(handle, &pid);
 
     if (status == BGWH_STOPPED) {
+        dsm_detach(seg);
         ereport(
             ERROR,
             (errcode(ERRCODE_INSUFFICIENT_RESOURCES),
@@ -211,4 +313,5 @@ LaunchWorker(chdbCopyContext* ctx) {
 
     /* Wait for it to finish. */
     WaitForBackgroundWorkerShutdown(handle);
+    dsm_detach(seg);
 }
